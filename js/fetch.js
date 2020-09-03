75231692aac7e624aaacdf38d116e4fb6cc8f696
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-09-03 17:45:50 +0300

    feat:add search location and fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,12 @@
 const locationInput = document.querySelector('#location-input');
+const searchIcon = document.querySelector('.search-icon ');
 
-async function fetchWeather() {
+searchIcon.addEventListener('click', async function fetchWeather() {
     try {
         console.log(locationInput.value);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=9c13504c0639e9b85fa4582326935900`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=9c13504c0639e9b85fa4582326935900`);
         const data = await response.json();
+        console.log(data);
         const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&
         exclude=hourly,daily&appid=9c13504c0639e9b85fa4582326935900`);
         const data2 = await secondResponse.json();
@@ -12,4 +14,4 @@ async function fetchWeather() {
     } catch (error) {
         return error;
     }
-}
+});
